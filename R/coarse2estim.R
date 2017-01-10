6fab920bfe6871b0c08c22af4fb5bf40705fdabc
Anne Cori <a.cori@imperial.ac.uk>
2017-01-10 14:58:33 +0000

    edited coarse2estim so upper quantile calculated for all posterior values rather than just the first one, and added in the Erlang distribution

--- R/coarse2estim.R
+++ R/coarse2estim.R
@@ -27,19 +27,24 @@ coarse2estim <- function(object, n_samples=1000){
   dist <- object@dist
   
   ##  Probability matrix that will be used in EpiEstim based on which distribution is specified by the user
-  if (dist == "G"){
-    max_interval <- 1:ceiling(qgamma(0.999, shape=object@ests[1,1], scale=object@ests[2,1]))
+  if (dist == "G" | dist == "E"){
+  	# For each input parameter set, find the 99th percentile, and take the maximum of these as the maximum
+    # serial interval that we need to consider
+    maxValue <- max( sapply(1:n_samples, function(i) ceiling(qgamma(0.999, shape = samples[i,1], scale = samples[i,2])) ) )
+    max_interval <- 1:maxValue
     prob_matrix <- apply(samples, 1, function(x) pgamma(max_interval+0.5, shape=x[1], scale=x[2]) - pgamma(max_interval-0.5, shape=x[1], scale=x[2]))
   } else if (dist == "off1G"){
     # offset gamma distribution with shifted min and max value of max serial interval
-    max_interval <- 0:ceiling(qgamma(0.999, shape=object@ests[1,1], scale=object@ests[2,1]))
+    maxValue <- max( sapply(1:n_samples, function(i) ceiling(qgamma(0.999, shape = samples[i,1], scale = samples[i,2])) ) )
+    max_interval <- 0:maxValue
     prob_matrix <- apply(samples, 1, function(x) pgamma(max_interval+0.5, shape=x[1], scale=x[2]) - pgamma(max_interval-0.5, shape=x[1], scale=x[2]))
-  }
-  else if (dist == "W"){
-    max_interval <- 1:ceiling(qweibull(0.999, shape=object@ests[1,1], scale=object@ests[2,1]))
+  } else if (dist == "W"){
+  	maxValue <- max( sapply(1:n_samples, function(i) ceiling(qweibull(0.999, shape = samples[i,1], scale = samples[i,2])) ) )
+    max_interval <- 1:maxValue
     prob_matrix <- apply(samples, 1, function(x) pweibull(max_interval+0.5, shape=x[1], scale=x[2]) - pweibull(max_interval-0.5, shape=x[1], scale=x[2]))
   } else if (dist == "L"){
-    max_interval <- 1:ceiling(qlnorm(0.999, meanlog=object@ests[1,1], sdlog=object@ests[2,1]))
+  	maxValue <- max( sapply(1:n_samples, function(i) ceiling(qlnorm(0.999, meanlog = samples[i,1], sdlog = samples[i,2])) ) )
+    max_interval <- 1:maxValue
     prob_matrix <- apply(samples, 1, function(x) plnorm(max_interval+0.5, meanlog=x[1], sdlog=x[2]) - plnorm(max_interval-0.5, meanlog=x[1], sdlog=x[2]))
   } else {
     stop(sprintf("Distribtion (%s) not supported",dist))
