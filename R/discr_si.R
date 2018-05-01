97a8a077d269885391793b23b93f4740aed3cf85
Anne Cori <a.cori@imperial.ac.uk>
2018-05-01 18:51:33 +0100

    edited discr_si function so it can be used with k a vector

--- R/discr_si.R
+++ R/discr_si.R
@@ -7,7 +7,7 @@
 #' 
 #' \code{discr_si} computes the discrete distribution of the serial interval, assuming that the serial interval is shifted Gamma distributed, with shift 1. 
 #' 
-#' @param k Positive integer for which the discrete distribution is desired.
+#' @param k Positive integer, or vector of positive ingerers for which the discrete distribution is desired.
 #' @param mu A positive real giving the mean of the Gamma distribution.
 #' @param sigma A non-negative real giving the standard deviation of the Gamma distribution.
 #' @return Gives the discrete probability \eqn{w_k} that the serial interval is equal to \eqn{k}.
@@ -28,24 +28,26 @@
 #' ## Computing the discrete serial interval of influenza
 #' mean_flu_si <- 2.6
 #' sd_flu_si <- 1.5
-#' dicrete_si_distr <- sapply(0:20, function(i) discr_si(i, mean_flu_si, sd_flu_si))
-#' plot(0:20, dicrete_si_distr, type="h", lwd=10, lend=1, xlab="time (days)", ylab="frequency")
-#' title(main="Discrete distribution of the serial interval of influenza")
+#' dicrete_si_distr <- discr_si(0:20, mean_flu_si, sd_flu_si)
+#' plot(0:20, dicrete_si_distr, type = "h", lwd = 10, lend = 1, xlab = "time (days)", ylab = "frequency")
+#' title(main = "Discrete distribution of the serial interval of influenza")
 discr_si <- function(k, mu, sigma) ### TO DO: make sure this also works if k is a vector
 {
-  if(sigma<0)
+  if(sigma < 0)
     stop("sigma must be >=0.")
-  
+  if(mu <= 1)
+    stop("mu must be >1")
+  if(any(k < 0))
+    stop("all values in k must be >=0.")
+   
   a <- ((mu-1)/sigma)^2
   b <- sigma^2/(mu-1)
   
-  cdf_gamma <- function(k, a, b)
-  {
-    pgamma(k, shape = a, scale = b)
-  }
+  cdf_gamma <- function(k, a, b) pgamma(k, shape = a, scale = b)
   
   res <- k*cdf_gamma(k,a,b)+(k-2)*cdf_gamma(k-2,a,b)-2*(k-1)*cdf_gamma(k-1,a,b)
   res <- res+a*b*(2*cdf_gamma(k-1,a+1,b)-cdf_gamma(k-2,a+1,b)-cdf_gamma(k,a+1,b))
-  res <- max(0,res)
+  res <- sapply(res, function(e) max(0,e))
+  
   return(res)
 }
