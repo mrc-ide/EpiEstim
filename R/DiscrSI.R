2f5782c9bef10d7f384d18a942f950f326cedfdc
Anne Cori <a.cori@imperial.ac.uk>
2018-05-01 18:31:20 +0100

    tidying code and examples for discr_si

--- R/DiscrSI.R
+++ R/DiscrSI.R
@@ -26,29 +26,26 @@
 #' @export
 #' @examples
 #' ## Computing the discrete serial interval of influenza
-#' MeanFluSI <- 2.6
-#' SdFluSI <- 1.5
-#' DicreteSIDistr <- vector()
-#' for(i in 0:20)
-#' {
-#' DicreteSIDistr[i+1] <- discr_si(i, MeanFluSI, SdFluSI)
-#' }
-#' plot(0:20, DicreteSIDistr, type="h", lwd=10, lend=1, xlab="time (days)", ylab="frequency")
+#' mean_flu_si <- 2.6
+#' sd_flu_si <- 1.5
+#' dicrete_si_distr <- sapply(0:20, function(i) discr_si(i, mean_flu_si, sd_flu_si))
+#' plot(0:20, dicrete_si_distr, type="h", lwd=10, lend=1, xlab="time (days)", ylab="frequency")
 #' title(main="Discrete distribution of the serial interval of influenza")
-discr_si<-function(k,mu,sigma)
+discr_si <- function(k, mu, sigma) ### TO DO: make sure this also works if k is a vector
 {
   if(sigma<0)
-  {
     stop("sigma must be >=0.")
-  }
-  a=((mu-1)/sigma)^2
-  b=sigma^2/(mu-1)
-  CDFGamma<-function(k,a,b)
+  
+  a <- ((mu-1)/sigma)^2
+  b <- sigma^2/(mu-1)
+  
+  cdf_gamma <- function(k, a, b)
   {
-    return(pgamma(k,shape=a,scale=b))
+    pgamma(k, shape = a, scale = b)
   }
-  res<-k*CDFGamma(k,a,b)+(k-2)*CDFGamma(k-2,a,b)-2*(k-1)*CDFGamma(k-1,a,b)
-  res<-res+a*b*(2*CDFGamma(k-1,a+1,b)-CDFGamma(k-2,a+1,b)-CDFGamma(k,a+1,b))
-  res<-max(0,res)
+  
+  res <- k*cdf_gamma(k,a,b)+(k-2)*cdf_gamma(k-2,a,b)-2*(k-1)*cdf_gamma(k-1,a,b)
+  res <- res+a*b*(2*cdf_gamma(k-1,a+1,b)-cdf_gamma(k-2,a+1,b)-cdf_gamma(k,a+1,b))
+  res <- max(0,res)
   return(res)
 }
