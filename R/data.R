2d032e114cf22b5181a7e4e564ba573c161c9bd4
Anne Cori <Anne@wpia-didelt156.home>
2018-05-04 10:57:33 +0100

    added data files as in Thompson et al. (in prep). Also corrected issues where dic.fit.mcmc has argument called init.pars, not init_pars as mistakenly changed to yesterday

--- R/data.R
+++ R/data.R
@@ -1,3 +1,49 @@
+#' Data on the 2009 H1N1 influenza pandemic in a school in New York city
+#'
+#' This data set gives:
+#' 1/ the daily incidence of self-reported and laboratory-confirmed cases of influenza 
+#' amongst children in a school in New York city during the 2009 H1N1 influenza pandemic (see source and references),
+#' 2/ interval-censored serial interval data from the 2009 outbreak of H1N1 influenza 
+#' in a New York city school (see references).
+#' 
+#' @name flu_2009_NYC_school
+#' @docType data
+#' @format A list of two elements: 
+#' \describe{
+#'   \item{incidence}{a dataframe with 14 lines containing dates in first column, and daily incidence in second column ,}
+#'   \item{si_data}{a dataframe containing data on the generation time for 16 pairs of infector/infected individuals 
+#'   (see references and see argument \code{si_data} of function \code{estimate_r} for details on columns) }
+#' }
+#' @source Lessler J. et al. (2009) Outbreak of 2009 pandemic influenza A (H1N1) at a New York City school. New Eng J Med 361: 2628-2636.
+#' @references 
+#' {
+#' Lessler J. et al. (2009) Outbreak of 2009 pandemic influenza A (H1N1) at a New York City school. New Eng J Med 361: 2628-2636. }
+#' @examples 
+#' \dontrun{
+#' ## Note the following examples use an MCMC routine 
+#' ## to estimate the serial interval distribution from data, 
+#' ## so they may take a few minutes to run
+#' 
+#' ## load data on pandemic flu in a New York school in 2009
+#' data("flu_2009_NYC_school")
+#' 
+#' ## estimate the reproduction number (method "si_from_data")
+#' estimate_r(flu_2009_NYC_school$incidence, method="si_from_data",
+#'          si_data = flu_2009_NYC_school$si_data,
+#'           config=list(t_start=2:8, t_end=8:14,
+#'                       si_parametric_distr = "G", 
+#'                       mcmc_control = list(burnin = 1000, 
+#'                                  thin=10, seed = 1), 
+#'                       n1 = 1000, n2 = 50,
+#'                       plot=TRUE)
+#'           )
+#' # the second plot produced shows, at each each day, 
+#' # the estimate of the reproduction number 
+#' # over the 7-day window finishing on that day.
+#' }
+NULL
+
+##################################################################################################
 #' Data on the 1918 H1N1 influenza pandemic in Baltimore.
 #'
 #' This data set gives:
@@ -50,15 +96,24 @@ NULL
 #' (ARI, defined as at least two symptoms among fever, cough, sore throat, and runny nose)
 #' amongst children in a school in Pennsylvania during the 2009 H1N1 influenza pandemic (see source and references),
 #' 2/ the discrete daily distribution of the serial interval for influenza, assuming a shifted Gamma distribution with mean 2.6 days, standard deviation 1.5 days and shift 1 day (see references).
+#' 3/ interval-censored serial interval data from the 2009 outbreak of H1N1 influenza 
+#' in San Antonio, Texas, USA (see references).
 #' 
 #' @name Flu2009
 #' @docType data
-#' @format A list of two elements: 
+#' @format A list of three elements: 
 #' \describe{
-#'   \item{incidence}{a dataframe with 32 lines containing dates in first column, and daily incidence in second column,}
-#'   \item{si_distr}{a vector containing a set of 12 probabilities.}
+#'   \item{incidence}{a dataframe with 32 lines containing dates in first column, and daily incidence in second column (Cauchemez et al., 2011),}
+#'   \item{si_distr}{a vector containing a set of 12 probabilities (Ferguson et al, 2005),}
+#'   \item{si_data}{a dataframe with 16 lines giving serial interval patient data collected in a household study in San Antonio, 
+#'   Texas throughout the 2009 H1N1 outbreak (Morgan et al., 2010). }
 #' }
-#' @source Cauchemez S. et al. (2011) Role of social networks in shaping disease transmission during a community outbreak of 2009 H1N1 pandemic influenza. Proc Natl Acad Sci U S A 108(7), 2825-2830.
+#' @source 
+#' {
+#' Cauchemez S. et al. (2011) Role of social networks in shaping disease transmission during a community outbreak of 2009 H1N1 pandemic influenza. Proc Natl Acad Sci U S A 108(7), 2825-2830.
+#' 
+#' Morgan O.W. et al. (2010) Household transmission of pandemic (H1N1) 2009, San Antonio, Texas, USA, April-May 2009. Emerg Infect Dis 16: 631-637. 
+#' }
 #' @references 
 #' {
 #' Cauchemez S. et al. (2011) Role of social networks in shaping disease transmission during a community outbreak of 2009 H1N1 pandemic influenza. Proc Natl Acad Sci U S A 108(7), 2825-2830.
@@ -78,6 +133,28 @@ NULL
 #' # the second plot produced shows, at each each day, 
 #' # the estimate of the reproduction number 
 #' # over the 7-day window finishing on that day.
+#' 
+#' \dontrun{
+#' ## Note the following examples use an MCMC routine 
+#' ## to estimate the serial interval distribution from data, 
+#' ## so they may take a few minutes to run
+#' 
+#' ## estimate the reproduction number (method "si_from_data")
+#' estimate_r(Flu2009$incidence, method="si_from_data",
+#'           si_data = Flu2009$si_data,
+#'           config=list(t_start=2:26, t_end=8:32, 
+#'                       mcmc_control = list(burnin = 1000, 
+#'                                  thin=10, seed = 1), 
+#'                       n1 = 1000, n2 = 50,
+#'                       si_parametric_distr = "G",
+#'                       plot=TRUE)
+#'           )
+#' # the second plot produced shows, at each each day, 
+#' # the estimate of the reproduction number 
+#' # over the 7-day window finishing on that day.
+#' }
+#' 
+#' 
 NULL
 
 ##################################################################################################
