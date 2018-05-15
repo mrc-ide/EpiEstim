9506005eeea85858a616908ea1b8390e70431638
Anne Cori <a.cori@imperial.ac.uk>
2018-05-15 13:29:47 +0100

    reformatting code using styler and to ensure lines are cut at 80 characters

--- R/check_cdt_samples_convergence.R
+++ R/check_cdt_samples_convergence.R
@@ -1,15 +1,20 @@
-#######################################################################################################################
-# check_cdt_samples_convergence runs a Gelman Rubin test to check convergence of the MCMC chain in coarseDataTools #
-#######################################################################################################################
+################################################################################
+# check_cdt_samples_convergence runs a Gelman Rubin test to check convergence of
+# the MCMC chain in coarseDataTools #
+################################################################################
 
 #' Checking convergence of an MCMC chain by using the Gelman-Rubin algorithm
 #'
-#' \code{check_cdt_samples_convergence} Checking convergence of an MCMC chain by using the Gelman-Rubin algorithm
+#' \code{check_cdt_samples_convergence} Checking convergence of an MCMC chain by
+#'  using the Gelman-Rubin algorithm
 #'
-#' @param cdt_samples the \code{@sample} slot of a \code{cd.fit.mcmc} S4 object (see package \code{coarseDataTools})
-#' @return TRUE if the Gelman Rubin test for convergence was successful, FALSE otherwise
+#' @param cdt_samples the \code{@sample} slot of a \code{cd.fit.mcmc} S4 object 
+#' (see package \code{coarseDataTools})
+#' @return TRUE if the Gelman Rubin test for convergence was successful, FALSE 
+#' otherwise
 #' @details{
-#' This function splits an MCMC chain in two halfs and uses the Gelman-Rubin algorithm to assess convergence of the chain by comparing its two halves.
+#' This function splits an MCMC chain in two halfs and uses the Gelman-Rubin 
+#' algorithm to assess convergence of the chain by comparing its two halves.
 #' }
 #' @seealso \code{\link{estimate_R}}
 #' @author Anne Cori
@@ -26,10 +31,10 @@
 #'
 #' ## estimate the serial interval from data
 #' SI_fit <- coarseDataTools::dic.fit.mcmc(dat = MockRotavirus$si_data,
-#'                              dist="G",
-#'                              init_pars=init_mcmc_params(MockRotavirus$si_data, "G"),
-#'                              burnin = 1000,
-#'                              n.samples = 5000)
+#'                      dist="G",
+#'                      init_pars=init_mcmc_params(MockRotavirus$si_data, "G"),
+#'                      burnin = 1000,
+#'                      n.samples = 5000)
 #'
 #' ## use check_cdt_samples_convergence to check convergence
 #' converg_diag <- check_cdt_samples_convergence(SI_fit@samples)
@@ -37,30 +42,29 @@
 #'
 #' }
 #'
-check_cdt_samples_convergence <- function(cdt_samples)
-{
-
-  #############################################################################################################################
-  # checking convergence of the MCMC by using the Gelman-Rubin algorithm between the first and second half of the MCMC sample
-  spl1 <- cdt_samples[1:floor(nrow(cdt_samples)/2),]
-  spl2 <- cdt_samples[(ceiling(nrow(cdt_samples)/2)+1):nrow(cdt_samples),]
+check_cdt_samples_convergence <- function(cdt_samples) {
+  ## checking convergence of the MCMC by using the Gelman-Rubin algorithm 
+  ## between the first and second half of the MCMC sample
+  spl1 <- cdt_samples[1:floor(nrow(cdt_samples) / 2), ]
+  spl2 <- cdt_samples[(ceiling(nrow(cdt_samples) / 2) + 1):nrow(cdt_samples), ]
   GRD <- gelman.diag(as.mcmc.list(list(as.mcmc(spl1), as.mcmc(spl2))))
-  # Is any of the potential scale reduction factors >1.1 (looking at the upper CI)?
+  # Is any of the potential scale reduction factors >1.1 
+  # (looking at the upper CI)?
   # If so this would suggest that the MCMC has not converged well.
-  if(any(GRD$psrf[,"Upper C.I."]>1.1))
-  {
-    warning("The Gelman-Rubin algorithm suggests the MCMC may not have converged within the number of iterations (MCMC.burnin + n1) specified.
+  if (any(GRD$psrf[, "Upper C.I."] > 1.1)) {
+    warning("The Gelman-Rubin algorithm suggests the MCMC may not have converged
+within the number of iterations (MCMC.burnin + n1) specified.
             You can visualise the full MCMC chain using: \n
             > par(mfrow=c(2,1))
-            > plot(res$SI.Moments[,'Mean'], type='l', xlab='Iterations', ylab='Mean SI')
-            > plot(res$SI.Moments[,'Std'], type='l', xlab='Iterations', ylab='Std SI'),
+            > plot(res$SI.Moments[,'Mean'], type='l', xlab='Iterations', 
+ylab='Mean SI')
+            > plot(res$SI.Moments[,'Std'], type='l', xlab='Iterations', 
+ylab='Std SI'),
             where res is the output of estimate_R
             and decide whether to rerun for longer.")
     return(FALSE)
-  }else
-  {
+  } else {
     cat("\nGelman-Rubin MCMC convergence diagnostic was successful.")
     return(TRUE)
   }
-
 }
--- R/data.R
+++ R/data.R
@@ -1,23 +1,29 @@
 #' Data on the 2009 H1N1 influenza pandemic in a school in New York city
 #'
 #' This data set gives:
-#' 1/ the daily incidence of self-reported and laboratory-confirmed cases of influenza
-#' amongst children in a school in New York city during the 2009 H1N1 influenza pandemic (see source and references),
-#' 2/ interval-censored serial interval data from the 2009 outbreak of H1N1 influenza
-#' in a New York city school (see references).
+#' 1/ the daily incidence of self-reported and laboratory-confirmed cases of 
+#' influenza amongst children in a school in New York city during the 2009 H1N1 
+#' influenza pandemic (see source and references),
+#' 2/ interval-censored serial interval data from the 2009 outbreak of H1N1 
+#' influenza in a New York city school (see references).
 #'
 #' @name flu_2009_NYC_school
 #' @docType data
 #' @format A list of two elements:
 #' \describe{
-#'   \item{incidence}{a dataframe with 14 lines containing dates in first column, and daily incidence in second column ,}
-#'   \item{si_data}{a dataframe containing data on the generation time for 16 pairs of infector/infected individuals
-#'   (see references and see argument \code{si_data} of function \code{estimate_R} for details on columns) }
+#'   \item{incidence}{a dataframe with 14 lines containing dates in first 
+#'   column, and daily incidence in second column ,}
+#'   \item{si_data}{a dataframe containing data on the generation time for 16 
+#'   pairs of infector/infected individuals
+#'   (see references and see argument \code{si_data} of function 
+#'   \code{estimate_R} for details on columns) }
 #' }
-#' @source Lessler J. et al. (2009) Outbreak of 2009 pandemic influenza A (H1N1) at a New York City school. New Eng J Med 361: 2628-2636.
+#' @source Lessler J. et al. (2009) Outbreak of 2009 pandemic influenza A (H1N1)
+#'  at a New York City school. New Eng J Med 361: 2628-2636.
 #' @references
 #' {
-#' Lessler J. et al. (2009) Outbreak of 2009 pandemic influenza A (H1N1) at a New York City school. New Eng J Med 361: 2628-2636. }
+#' Lessler J. et al. (2009) Outbreak of 2009 pandemic influenza A (H1N1) at a 
+#' New York City school. New Eng J Med 361: 2628-2636. }
 #' @examples
 #' \dontrun{
 #' ## Note the following examples use an MCMC routine
@@ -43,12 +49,15 @@
 #' }
 NULL
 
-##################################################################################################
+################################################################################
 #' Data on the 1918 H1N1 influenza pandemic in Baltimore.
 #'
 #' This data set gives:
-#' 1/ the daily incidence of onset of disease in Baltimore during the 1918 H1N1 influenza pandemic (see source and references),
-#' 2/ the discrete daily distribution of the serial interval for influenza, assuming a shifted Gamma distribution with mean 2.6 days, standard deviation 1.5 days and shift 1 day (see references).
+#' 1/ the daily incidence of onset of disease in Baltimore during the 1918 H1N1 
+#' influenza pandemic (see source and references),
+#' 2/ the discrete daily distribution of the serial interval for influenza, 
+#' assuming a shifted Gamma distribution with mean 2.6 days, standard deviation 
+#' 1.5 days and shift 1 day (see references).
 #'
 #' @name Flu1918
 #' @docType data
@@ -57,20 +66,29 @@ NULL
 #'   \item{incidence}{a vector containing 92 days of observation,}
 #'   \item{si_distr}{a vector containing a set of 12 probabilities.}
 #' }
-#' @source Frost W. and E. Sydenstricker (1919) Influenza in Maryland: preliminary statistics of certain localities. Public Health Rep.(34): 491-504.
+#' @source Frost W. and E. Sydenstricker (1919) Influenza in Maryland: 
+#' preliminary statistics of certain localities. 
+#' Public Health Rep.(34): 491-504.
 #' @references
 #' {
-#' Cauchemez S. et al. (2011) Role of social networks in shaping disease transmission during a community outbreak of 2009 H1N1 pandemic influenza. Proc Natl Acad Sci U S A 108(7), 2825-2830.
+#' Cauchemez S. et al. (2011) Role of social networks in shaping disease 
+#' transmission during a community outbreak of 2009 H1N1 pandemic influenza. 
+#' Proc Natl Acad Sci U S A 108(7), 2825-2830.
 #'
-#' Ferguson N.M. et al. (2005) Strategies for containing an emerging influenza pandemic in Southeast Asia. Nature 437(7056), 209-214.
+#' Ferguson N.M. et al. (2005) Strategies for containing an emerging influenza 
+#' pandemic in Southeast Asia. Nature 437(7056), 209-214.
 #'
-#' Fraser C. et al. (2011) Influenza Transmission in Households During the 1918 Pandemic. Am J Epidemiol 174(5): 505-514.
+#' Fraser C. et al. (2011) Influenza Transmission in Households During the 1918 
+#' Pandemic. Am J Epidemiol 174(5): 505-514.
 #'
-#' Frost W. and E. Sydenstricker (1919) Influenza in Maryland: preliminary statistics of certain localities. Public Health Rep.(34): 491-504.
+#' Frost W. and E. Sydenstricker (1919) Influenza in Maryland: preliminary 
+#' statistics of certain localities. Public Health Rep.(34): 491-504.
 #'
-#' Vynnycky E. et al. (2007) Estimates of the reproduction numbers of Spanish influenza using morbidity data. Int J Epidemiol 36(4): 881-889.
+#' Vynnycky E. et al. (2007) Estimates of the reproduction numbers of Spanish 
+#' influenza using morbidity data. Int J Epidemiol 36(4): 881-889.
 #'
-#' White L.F. and M. Pagano (2008) Transmissibility of the influenza virus in the 1918 pandemic. PLoS One 3(1): e1498.
+#' White L.F. and M. Pagano (2008) Transmissibility of the influenza virus in 
+#' the 1918 pandemic. PLoS One 3(1): e1498.
 #' }
 #' @examples
 #' ## load data on pandemic flu in Baltimore in 1918
@@ -87,43 +105,48 @@ NULL
 #' # over the 7-day window finishing on that day.
 NULL
 
-##################################################################################################
+################################################################################
 
 #' Data on the 2009 H1N1 influenza pandemic in a school in Pennsylvania.
 #'
 #' This data set gives:
 #' 1/ the daily incidence of onset of acute respiratory illness
-#' (ARI, defined as at least two symptoms among fever, cough, sore throat, and runny nose)
-#' amongst children in a school in Pennsylvania during the 2009 H1N1 influenza pandemic (see source and references),
-#' 2/ the discrete daily distribution of the serial interval for influenza, assuming a shifted Gamma distribution with mean 2.6 days, standard deviation 1.5 days and shift 1 day (see references).
-#' 3/ interval-censored serial interval data from the 2009 outbreak of H1N1 influenza
+#' (ARI, defined as at least two symptoms among fever, cough, sore throat, and 
+#' runny nose)
+#' amongst children in a school in Pennsylvania during the 2009 H1N1 influenza 
+#' pandemic (see source and references),
+#' 2/ the discrete daily distribution of the serial interval for influenza, 
+#' assuming a shifted Gamma distribution with mean 2.6 days, standard deviation 
+#' 1.5 days and shift 1 day (see references).
+#' 3/ interval-censored serial interval data from the 2009 outbreak of H1N1 
+#' influenza
 #' in San Antonio, Texas, USA (see references).
 #'
 #' @name Flu2009
 #' @docType data
-#' @format A list of three elements:
-#' \describe{
-#'   \item{incidence}{a dataframe with 32 lines containing dates in first column, and daily incidence in second column (Cauchemez et al., 2011),}
-#'   \item{si_distr}{a vector containing a set of 12 probabilities (Ferguson et al, 2005),}
-#'   \item{si_data}{a dataframe with 16 lines giving serial interval patient data collected in a household study in San Antonio,
-#'   Texas throughout the 2009 H1N1 outbreak (Morgan et al., 2010). }
-#' }
-#' @source
-#' {
-#' Cauchemez S. et al. (2011) Role of social networks in shaping disease transmission during a community outbreak of 2009 H1N1 pandemic influenza. Proc Natl Acad Sci U S A 108(7), 2825-2830.
-#'
-#' Morgan O.W. et al. (2010) Household transmission of pandemic (H1N1) 2009, San Antonio, Texas, USA, April-May 2009. Emerg Infect Dis 16: 631-637.
-#' }
-#' @references
-#' {
-#' Cauchemez S. et al. (2011) Role of social networks in shaping disease transmission during a community outbreak of 2009 H1N1 pandemic influenza. Proc Natl Acad Sci U S A 108(7), 2825-2830.
-#'
-#' Ferguson N.M. et al. (2005) Strategies for containing an emerging influenza pandemic in Southeast Asia. Nature 437(7056), 209-214.
-#' }
+#' @format A list of three elements: \describe{ \item{incidence}{a dataframe
+#'   with 32 lines containing dates in first column, and daily incidence in
+#'   second column (Cauchemez et al., 2011),} \item{si_distr}{a vector
+#'   containing a set of 12 probabilities (Ferguson et al, 2005),} 
+#'   \item{si_data}{a dataframe with 16 lines giving serial interval patient
+#'   data collected in a household study in San Antonio, Texas throughout the
+#'   2009 H1N1 outbreak (Morgan et al., 2010). } }
+#' @source { Cauchemez S. et al. (2011) Role of social networks in shaping
+#' disease transmission during a community outbreak of 2009 H1N1 pandemic
+#' influenza. Proc Natl Acad Sci U S A 108(7), 2825-2830.
+#' 
+#' Morgan O.W. et al. (2010) Household transmission of pandemic (H1N1) 2009, San
+#' Antonio, Texas, USA, April-May 2009. Emerg Infect Dis 16: 631-637. }
+#' @references { Cauchemez S. et al. (2011) Role of social networks in shaping
+#' disease transmission during a community outbreak of 2009 H1N1 pandemic
+#' influenza. Proc Natl Acad Sci U S A 108(7), 2825-2830.
+#' 
+#' Ferguson N.M. et al. (2005) Strategies for containing an emerging influenza
+#' pandemic in Southeast Asia. Nature 437(7056), 209-214. }
 #' @examples
 #' ## load data on pandemic flu in a school in 2009
 #' data("Flu2009")
-#'
+#' 
 #' ## estimate the reproduction number (method "non_parametric_si")
 #' estimate_R(Flu2009$incidence, method="non_parametric_si",
 #'           config=list(t_start=2:26, t_end=8:32,
@@ -133,7 +156,7 @@ NULL
 #' # the second plot produced shows, at each each day,
 #' # the estimate of the reproduction number
 #' # over the 7-day window finishing on that day.
-#'
+#' 
 #' \dontrun{
 #' ## Note the following examples use an MCMC routine
 #' ## to estimate the serial interval distribution from data,
@@ -157,14 +180,16 @@ NULL
 #'
 NULL
 
-##################################################################################################
+################################################################################
 
 #' Data on the 1861 measles epidemic in Hagelloch, Germany.
-#'
-#' This data set gives:
-#' 1/ the daily incidence of onset of symptoms in Hallegoch (Germany) during the 1861 measles epidemic (see source and references),
-#' 2/ the discrete daily distribution of the serial interval for measles, assuming a shifted Gamma distribution with mean 14.9 days, standard deviation 3.9 days and shift 1 day (see references).
-#'
+#' 
+#' This data set gives: 1/ the daily incidence of onset of symptoms in Hallegoch
+#' (Germany) during the 1861 measles epidemic (see source and references), 2/
+#' the discrete daily distribution of the serial interval for measles, assuming
+#' a shifted Gamma distribution with mean 14.9 days, standard deviation 3.9 days
+#' and shift 1 day (see references).
+#' 
 #' @name Measles1861
 #' @docType data
 #' @format A list of two elements:
@@ -172,8 +197,11 @@ NULL
 #'   \item{incidence}{a vector containing 48 days of observation,}
 #'   \item{si_distr}{a vector containing a set of 38 probabilities.}
 #' }
-#' @source Groendyke C. et al. (2011) Bayesian Inference for Contact Networks Given Epidemic Data. Scandinavian Journal of Statistics 38(3): 600-616.
-#' @references Groendyke C. et al. (2011) Bayesian Inference for Contact Networks Given Epidemic Data. Scandinavian Journal of Statistics 38(3): 600-616.
+#' @source Groendyke C. et al. (2011) Bayesian Inference for Contact Networks 
+#' Given Epidemic Data. Scandinavian Journal of Statistics 38(3): 600-616.
+#' @references Groendyke C. et al. (2011) Bayesian Inference for Contact 
+#' Networks Given Epidemic Data. Scandinavian Journal of Statistics 38(3): 
+#' 600-616.
 #' @examples
 #' ## load data on measles in Hallegoch in 1861
 #' data("Measles1861")
@@ -189,13 +217,15 @@ NULL
 #' # over the 7-day window finishing on that day.
 NULL
 
-##################################################################################################
+################################################################################
 
 #' Data on the 2003 SARS epidemic in Hong Kong.
-#'
-#' This data set gives:
-#' 1/ the daily incidence of onset of symptoms in Hong Kong during the 2003 severe acute respiratory syndrome (SARS) epidemic (see source and references),
-#' 2/ the discrete daily distribution of the serial interval for SARS, assuming a shifted Gamma distribution with mean 8.4 days, standard deviation 3.8 days and shift 1 day (see references).
+#' 
+#' This data set gives: 1/ the daily incidence of onset of symptoms in Hong Kong
+#' during the 2003 severe acute respiratory syndrome (SARS) epidemic (see source
+#' and references), 2/ the discrete daily distribution of the serial interval
+#' for SARS, assuming a shifted Gamma distribution with mean 8.4 days, standard
+#' deviation 3.8 days and shift 1 day (see references).
 #'
 #' @name SARS2003
 #' @docType data
@@ -204,12 +234,17 @@ NULL
 #'   \item{incidence}{a vector containing 107 days of observation,}
 #'   \item{si_distr}{a vector containing a set of 25 probabilities.}
 #' }
-#' @source Cori A. et al. (2009) Temporal variability and social heterogeneity in disease transmission: the case of SARS in Hong Kong. PLoS Comput Biol 5(8): e1000471.
+#' @source Cori A. et al. (2009) Temporal variability and social heterogeneity 
+#' in disease transmission: the case of SARS in Hong Kong. PLoS Comput Biol 5(8)
+#' : e1000471.
 #' @references
 #' {
-#' Cori A. et al. (2009) Temporal variability and social heterogeneity in disease transmission: the case of SARS in Hong Kong. PLoS Comput Biol 5(8): e1000471.
+#' Cori A. et al. (2009) Temporal variability and social heterogeneity in 
+#' disease transmission: the case of SARS in Hong Kong. PLoS Comput Biol 5(8): 
+#' e1000471.
 #'
-#' Lipsitch M. et al. (2003) Transmission dynamics and control of severe acute respiratory syndrome. Science 300(5627): 1966-1970.
+#' Lipsitch M. et al. (2003) Transmission dynamics and control of severe acute 
+#' respiratory syndrome. Science 300(5627): 1966-1970.
 #' }
 #' @examples
 #' ## load data on SARS in Hong Kong in 2003
@@ -226,13 +261,15 @@ NULL
 #' # over the 7-day window finishing on that day.
 NULL
 
-##################################################################################################
+################################################################################
 
 #' Data on the 1972 smallpox epidemic in Kosovo
-#'
-#' This data set gives:
-#' 1/ the daily incidence of onset of symptoms in Kosovo during the 1972 smallpox epidemic (see source and references),
-#' 2/ the discrete daily distribution of the serial interval for smallpox, assuming a shifted Gamma distribution with mean 22.4 days, standard deviation 6.1 days and shift 1 day (see references).
+#' 
+#' This data set gives: 1/ the daily incidence of onset of symptoms in Kosovo
+#' during the 1972 smallpox epidemic (see source and references), 2/ the
+#' discrete daily distribution of the serial interval for smallpox, assuming a
+#' shifted Gamma distribution with mean 22.4 days, standard deviation 6.1 days
+#' and shift 1 day (see references).
 #'
 #' @name Smallpox1972
 #' @docType data
@@ -241,14 +278,18 @@ NULL
 #'   \item{incidence}{a vector containing 57 days of observation,}
 #'   \item{si_distr}{a vector containing a set of 46 probabilities.}
 #' }
-#' @source Fenner F. et al. (1988) Smallpox and its Eradication. Geneva, World Health Organization.
+#' @source Fenner F. et al. (1988) Smallpox and its Eradication. Geneva, World 
+#' Health Organization.
 #' @references
 #' {
-#' Fenner F. et al. (1988) Smallpox and its Eradication. Geneva, World Health Organization.
+#' Fenner F. et al. (1988) Smallpox and its Eradication. Geneva, World Health 
+#' Organization.
 #'
-#' Gani R. and S. Leach (2001) Transmission potential of smallpox in contemporary populations. Nature 414(6865): 748-751.
+#' Gani R. and S. Leach (2001) Transmission potential of smallpox in 
+#' contemporary populations. Nature 414(6865): 748-751.
 #'
-#' Riley S. and N. M. Ferguson (2006) Smallpox transmission and control: spatial dynamics in Great Britain. Proc Natl Acad Sci U S A 103(33): 12637-12642.
+#' Riley S. and N. M. Ferguson (2006) Smallpox transmission and control: spatial
+#'  dynamics in Great Britain. Proc Natl Acad Sci U S A 103(33): 12637-12642.
 #' }
 #' @examples
 #' ## load data on smallpox in Kosovo in 1972
@@ -265,20 +306,25 @@ NULL
 #' # over the 7-day window finishing on that day.
 NULL
 
-##################################################################################################
+################################################################################
 
 #' Mock data on a rotavirus epidemic.
-#'
-#' This data set gives:
-#' 1/ the daily incidence of onset of symptoms in a mock outbreak of rotavirus ,
-#' 2/ mock observations of symptom onset dates for 19 pairs of infector/infected individuals.
+#' 
+#' This data set gives: 1/ the daily incidence of onset of symptoms in a mock
+#' outbreak of rotavirus , 2/ mock observations of symptom onset dates for 19
+#' pairs of infector/infected individuals.
 #'
 #' @name MockRotavirus
 #' @docType data
 #' @format A list of two elements:
 #' \describe{
 #'   \item{incidence}{a vector containing 53 days of observation,}
-#'   \item{si_distr}{a dataframe containing a set of 19 observations; each observation corresponds to a pair of infector/infected individuals. EL and ER columns contain the lower an upper bounds of the dates of symptoms onset in the infectors. SL and SR columns contain the lower an upper bounds of the dates of symptoms onset in the infected indiviuals. The type column corresponds to XXX TO BE COMPLETED XXX}
+#'   \item{si_distr}{a dataframe containing a set of 19 observations; each 
+#'   observation corresponds to a pair of infector/infected individuals. EL and 
+#'   ER columns contain the lower an upper bounds of the dates of symptoms onset
+#'    in the infectors. SL and SR columns contain the lower an upper bounds of 
+#'    the dates of symptoms onset in the infected indiviuals. The type column 
+#'    corresponds to XXX TO BE COMPLETED XXX}
 #' }
 #' @source XXX TO BE COMPLETED XXX
 #' @references
@@ -310,5 +356,3 @@ NULL
 #' # over the 7-day window finishing on that day.
 #' }
 NULL
-
-
