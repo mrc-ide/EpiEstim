eca4ecfd2012b4a64f6cac912b0ed16445ce7d2d
Anne Cori <a.cori@imperial.ac.uk>
2022-06-28 12:52:52 +0100

    new test for non parametric mode

--- tests/testthat/test-weekly.R
+++ tests/testthat/test-weekly.R
@@ -29,9 +29,10 @@ test_that("aggregating incidence works", {
 
 test_that("weekly version of estimate_R works in parametric mode", {
   
-  ## estimate the reproduction number (method "non_parametric_si")
+  ## estimate the reproduction number (method "parametric_si")
   ## when not specifying t_start and t_end in config, they are set to estimate
-  ## the reproduction number on sliding weekly windows                          
+  ## the reproduction number on sliding weekly windows      
+  method <- "parametric_si"
   config <- make_config(list(mean_si = mean_si,
                              std_si = std_si))
   res_weekly <- estimate_R_agg(incid = weekly_inc, 
@@ -39,12 +40,12 @@ test_that("weekly version of estimate_R works in parametric mode", {
                         dt_out = 7, # desired sliding window length
                         iter = 10,
                         config = config,
-                        method = "parametric_si",
+                        method = method,
                         grid = list(precision = 0.001, min = -1, max = 1))
   
   res_daily <- estimate_R(incid = incid, 
                                config = config,
-                               method = "parametric_si")
+                               method = method)
   
   ######################################################################
   ## test that the weekly incidence matches the aggregated daily one
@@ -77,7 +78,7 @@ test_that("weekly version of estimate_R works in parametric mode", {
   
   res_daily_reconstructed <- estimate_R(incid = res_weekly$I, 
                           config = config,
-                          method = "parametric_si")
+                          method = method)
   
   common_t_start <- intersect(res_daily_reconstructed$R$t_start, res_weekly$R$t_start)
   
@@ -87,5 +88,69 @@ test_that("weekly version of estimate_R works in parametric mode", {
   
   expect_true(all(relative_error < 1e-9)) 
                           
+})
+
+
+
+test_that("weekly version of estimate_R works in  non-parametric mode", {
+  
+  ## estimate the reproduction number (method "non_parametric_si")
+  ## when not specifying t_start and t_end in config, they are set to estimate
+  ## the reproduction number on sliding weekly windows     
+  method <- "non_parametric_si"
+  config <- make_config(list(si_distr = si_distr))
+  res_weekly <- estimate_R_agg(incid = weekly_inc, 
+                               dt = 7, # aggregation window of the data
+                               dt_out = 7, # desired sliding window length
+                               iter = 10,
+                               config = config,
+                               method = method,
+                               grid = list(precision = 0.001, min = -1, max = 1))
+  
+  res_daily <- estimate_R(incid = incid, 
+                          config = config,
+                          method = method)
+  
+  ######################################################################
+  ## test that the weekly incidence matches the aggregated daily one
+  ## except for first time window where we impose I = 1 on first day
+  ## TODO: explore the issue above of forcing I = 1 on first day
+  
+  for(i in seq_len(floor(length(res_daily$I) / dt))[-1])
+  {
+    expect_equal(sum(res_daily$I[i*dt+(1:dt)]), 
+                 sum(res_weekly$I[i*dt+(1:dt)]))
+  }
+  
+  ######################################################################
+  ## test that the weekly R estimates vaguely match if you use the daily data
+  ## or the weekly data
+  
+  common_t_start <- intersect(res_daily$R$t_start, res_weekly$R$t_start)
+  
+  relative_error <- abs(res_daily$R$`Mean(R)`[res_daily$R$t_start %in% common_t_start] -   
+                          res_weekly$R$`Mean(R)`[res_weekly$R$t_start %in% common_t_start]) / 
+    res_daily$R$`Mean(R)`[res_daily$R$t_start %in% common_t_start]
+  
+  ## only check after the 10th common_t_start as before hand there is a lot of
+  ## day to day variation
+  expect_true(all(relative_error[-c(1:20)] < 0.4)) # 0.4 arbitrarily small
+  
+  ######################################################################
+  ## test that the weekly R estimates from weekly data match exactly the weekly
+  ## R estimates from the daily reconstructed incidence
+  
+  res_daily_reconstructed <- estimate_R(incid = res_weekly$I, 
+                                        config = config,
+                                        method = method)
+  
+  common_t_start <- intersect(res_daily_reconstructed$R$t_start, res_weekly$R$t_start)
+  
+  relative_error <- abs(res_daily_reconstructed$R$`Mean(R)`[res_daily_reconstructed$R$t_start %in% common_t_start] -   
+                          res_weekly$R$`Mean(R)`[res_weekly$R$t_start %in% common_t_start]) / 
+    res_daily_reconstructed$R$`Mean(R)`[res_daily_reconstructed$R$t_start %in% common_t_start]
+  
+  expect_true(all(relative_error < 1e-9)) 
+  
 })
   
