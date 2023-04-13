06c7463507efda00354479aa24c672ae9750f3b5
Hugo Gruson <Bisaloo@users.noreply.github.com>
2023-04-13 12:55:48 +0200

    Use toString() rather than paste(collapse = ", ") Flagged by lintr::paste_linter()

--- R/utilities.R
+++ R/utilities.R
@@ -443,7 +443,7 @@ vcapply <- function(X, FUN, ...) {
 modify_defaults <- function(defaults, x, strict = TRUE) {
   extra <- setdiff(names(x), names(defaults))
   if (strict && (length(extra) > 0L)) {
-    stop("Additional invalid options: ", paste(extra, collapse=", "))
+    stop("Additional invalid options: ", toString(extra))
   }
   utils::modifyList(defaults, x, keep.null = TRUE) # keep.null is needed here
 }
