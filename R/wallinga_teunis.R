42eea5078691daf12c39f7f538e803da10a8edd5
Hugo Gruson <Bisaloo@users.noreply.github.com>
2023-04-13 12:51:44 +0200

    Use any() to quickly check if a condition is TRUE at least once Flagged by lintr::boolean_arithmetic_linter()

--- R/wallinga_teunis.R
+++ R/wallinga_teunis.R
@@ -123,7 +123,7 @@ wallinga_teunis <- function(incid,
     res <- vector()
     for (t in seq_len(T))
     {
-      if (length(which(Onset == t)) > 0) {
+      if (any(Onset == t)) {
         if (length(possible_ances_time[[t]]) > 0) {
           prob <- config$si_distr[t - possible_ances_time[[t]] + 1] *
             incid[possible_ances_time[[t]]]
