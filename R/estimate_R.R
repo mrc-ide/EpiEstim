31172df212c22d8b0e73fdfa0643c423988eb452
Anne Cori <a.cori@imperial.ac.uk>
2024-08-29 15:44:52 +0200

    correcting a couple of bugs and adding a minimum version for patchwork package

--- R/estimate_R.R
+++ R/estimate_R.R
@@ -440,7 +440,7 @@ estimate_R <- function(incid,
   )
   
   # Add extra fields based on method
-  if(method == "si_from_sample"){
+  if(method == "si_from_data"){
     out[["MCMC_converged"]] <- MCMC_conv
   }
 
