4cd098bf7c03eaeb93ee09db100d8bfd019f41ba
Steffi LaZerte <steffi@steffi.ca>
2025-11-12 17:39:29 -0600

    Clean up - gibbs_draws - default_mcmc_controls()     - Use markdown     - typo fixes

--- R/gibbs_draws.R
+++ R/gibbs_draws.R
@@ -114,28 +114,23 @@ default_priors <- function() {
        R = list(shape = 0.04, scale = 25))
 }
 
-
 #' Set default for MCMC control
 #'
 #' @return a list of default MCMC control parameters, containing:
-#'
-#' - n_iter: the number if iterations of the MCMC to perform
-#'
-#' - burnin: the burnin to use; MCMC iterations will only be recorded after
+#' - `n_iter`: the number of iterations for the MCMC to perform
+#' - `burnin`: the burnin to use; MCMC iterations will only be recorded after
 #'   the burnin
+#' - `thin`: MCMC iterations will only be recorded after the burnin and every 
+#'   `thin` iteration
 #'
-#' - thin: MCMC iterations will only be recorded after
-#'   the burnin and every `thin` iteration
-#'
-#'   Values can then be manually be edited as in the examples below.
+#' Values can then be manually edited as in the examples below.
 #'
 #' @export
 #'
 #' @examples
-#' mcmc_control<- default_mcmc_controls()
+#' mcmc_control <- default_mcmc_controls()
 #' # change to run for 10 times longer
 #' mcmc_control$n_iter <- mcmc_control$n_iter * 10
-#'
 
 default_mcmc_controls <- function() {
   list(n_iter = 1100L,
