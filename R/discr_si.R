0ad829cb1fb137a7b49c6a2f8a35df2e72437230
Steffi LaZerte <steffi@steffi.ca>
2025-11-12 14:57:11 -0600

    Clean up - discr_si - Use Markdown - Consolidate title, description and details - Clean up example

--- R/discr_si.R
+++ R/discr_si.R
@@ -1,23 +1,10 @@
-#########################################################
-# Discretized serial interval (assuming a shifted gamma #
-# distribution (with shift 1)                           #
-#########################################################
-
-#' Discretized Generation Time Distribution Assuming A Shifted Gamma 
-#' Distribution
+#' Compute discretized generation time distribution
 #'
-#' \code{discr_si} computes the discrete distribution of the serial interval, 
-#' assuming that the serial interval is shifted Gamma distributed, with shift 1.
-#'
-#' @param k Positive integer, or vector of positive integers for which the 
-#' discrete distribution is desired.
-#' @param mu A positive real giving the mean of the Gamma distribution.
-#' @param sigma A non-negative real giving the standard deviation of the Gamma 
-#' distribution.
-#' @return Gives the discrete probability \eqn{w_k} that the serial interval is 
-#' equal to \eqn{k}.
-#' @details{
-#' Assuming that the serial interval is shifted Gamma distributed with mean 
+#' Compute the discrete distribution of the serial interval, 
+#' assuming that the serial interval is a shifted Gamma distributed, 
+#' with shift 1.
+#' 
+#' Assuming that the serial interval is a shifted Gamma distributed with mean 
 #' \eqn{\mu}, standard deviation \eqn{\sigma} and shift \eqn{1},
 #' the discrete probability \eqn{w_k} that the serial interval is equal to 
 #' \eqn{k} is:
@@ -33,20 +20,34 @@
 #'
 #' where \eqn{F_{\{\mu,\sigma\}}} is the cumulative density function of a Gamma 
 #' distribution with mean \eqn{\mu} and standard deviation \eqn{\sigma}.
-#' }
-#' @seealso \code{\link{overall_infectivity}}, \code{\link{estimate_R}}
+#'
+#' @param k Positive integer, or vector of positive integers for which the 
+#' discrete distribution is desired.
+#' @param mu A positive real giving the mean of the Gamma distribution.
+#' @param sigma A non-negative real giving the standard deviation of the Gamma 
+#' distribution.
+#' 
+#' @return Gives the discrete probability \eqn{w_k} that the serial interval is 
+#' equal to \eqn{k}.
+#' 
+#' @seealso [overall_infectivity()], [estimate_R()]
 #' @author Anne Cori \email{a.cori@imperial.ac.uk}
-#' @references Cori, A. et al. A new framework and software to estimate 
-#' time-varying reproduction numbers during epidemics (AJE 2013).
+#' 
+#' @references 
+#' Cori, A. et al. A new framework and software to estimate time-varying 
+#' reproduction numbers during epidemics (AJE 2013).
+#' 
 #' @export
+#' 
 #' @examples
 #' ## Computing the discrete serial interval of influenza
 #' mean_flu_si <- 2.6
 #' sd_flu_si <- 1.5
 #' dicrete_si_distr <- discr_si(seq(0, 20), mean_flu_si, sd_flu_si)
 #' plot(seq(0, 20), dicrete_si_distr, type = "h",
-#'           lwd = 10, lend = 1, xlab = "time (days)", ylab = "frequency")
+#'      lwd = 10, lend = 1, xlab = "time (days)", ylab = "frequency")
 #' title(main = "Discrete distribution of the serial interval of influenza")
+
 discr_si <- function(k, mu, sigma) 
 {
   if (sigma < 0) {
