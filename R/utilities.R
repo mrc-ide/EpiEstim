412b49833a3e67a897dd3e83619e8634ed19e01a
rebeccanash <r.nash@imperial.ac.uk>
2026-03-25 12:10:13 +0000

    remove processing for incidence2 groups

--- R/utilities.R
+++ R/utilities.R
@@ -124,13 +124,8 @@ process_I_vector <- function(incid) {
   if (inherits(incid, "incidence2")) {
     I_col <- incidence2::get_count_value_name(incid)
     date_col <- incidence2::get_date_index_name(incid)
-    if ("group" %in% names(incid) && "local" %in% incid$group) {
-      incid <- incid[[I_col]][incid$group == "local"]
-      } else {
-        # no grouping or no local/imported distinction then sum all counts
-        dates <- unique(incid[[date_col]])
-        incid <- tapply(incid[[I_col]], incid[[date_col]], sum)[as.character(dates)]
-        }
+    dates <- unique(incid[[date_col]])
+    incid <- tapply(incid[[I_col]], incid[[date_col]], sum)[as.character(dates)]
     return(as.vector(incid))
     }
   # incidence object, the incident counts are being forced into a vector
