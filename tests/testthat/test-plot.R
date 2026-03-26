44b2610ebe509a7fd55f3b06b056ebe09999084b
Steffi LaZerte <steffi@steffi.ca>
2026-03-26 09:56:57 -0500

    Fix vdiffr tests broken (fixes #106)

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -20,36 +20,46 @@ R_c <- wallinga_teunis(
                 )
 )
 
-test_that("plot.estimate_R doesn't have to include the legend", {
-  skip("vdiffr tests not working - see #106")
-  skip_if_not_installed("vdiffr")
-  vdiffr::expect_doppelganger("Flu2009-instantaneous-no-legend", 
-                              plot(R_i, legend = FALSE))
+# Create snapshots by system, prevent fragility across developers
+system <- Sys.info()[["sysname"]]
 
+test_that("plot.estimate_R doesn't have to include the legend", {
+  skip_if_not_installed("vdiffr")
+  vdiffr::expect_doppelganger(
+    "Flu2009-instantaneous-no-legend", 
+    plot(R_i, legend = FALSE),
+    variant = system
+  )
 })
 
 test_that("incidence can be plotted separately with imported cases", {
-  skip("vdiffr tests not working - see #106")
   skip_if_not_installed("vdiffr")
-  vdiffr::expect_doppelganger("Flu2009-incidence-import", 
-                              plot(R_i, "incid", add_imported_cases=TRUE))
-
+  vdiffr::expect_doppelganger(
+    "Flu2009-incidence-import", 
+    plot(R_i, "incid", add_imported_cases=TRUE),
+    variant = system
+  )
 })
 
 test_that("serial interval distribution can be plotted separately", {
-  skip("vdiffr tests not working - see #106")
   skip_if_not_installed("vdiffr")
-  vdiffr::expect_doppelganger("Flu2009-SI", 
-                              plot(R_i, "SI"))
-
+  vdiffr::expect_doppelganger(
+    "Flu2009-SI", 
+    plot(R_i, "SI"),
+    variant = system
+  )
 })
 
 test_that("Reproduction numbers can be plotted separately", {
-  skip("vdiffr tests not working - see #106")
   skip_if_not_installed("vdiffr")
-  vdiffr::expect_doppelganger("Flu2009-Ri", 
-                              plot(R_i, "R", options_R = list(ylim = c(0, 4))))
-  vdiffr::expect_doppelganger("Flu2009-Rc",
-                              plot(R_c, "R", options_R = list(ylim = c(0, 4))))
-
+  vdiffr::expect_doppelganger(
+    "Flu2009-Ri", 
+    plot(R_i, "R", options_R = list(ylim = c(0, 4))),
+    variant = system
+  )
+  vdiffr::expect_doppelganger(
+    "Flu2009-Rc",
+    plot(R_c, "R", options_R = list(ylim = c(0, 4))),
+    variant = system
+  )
 })
