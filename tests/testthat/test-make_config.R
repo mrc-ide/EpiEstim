2ff635af34631b115c5825e078c40d1eaafc55df
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2026-05-02 06:08:44 +0100

    Test for warning if local[1] > 0

--- tests/testthat/test-make_config.R
+++ tests/testthat/test-make_config.R
@@ -13,3 +13,10 @@ test_that("make_config triggers process_I error for invalid incid columns", {
     make_config(incid = incid),
     "incid must be a vector or a dataframe with either i\\) a column")
 })
+
+test_that("make_config triggers process_I warning when incid$local[1] > 0", {
+  incid <- data.frame(local = c(3, 1, 0, 2), imported = c(0, 0, 0, 0))
+  expect_warning(
+    make_config(incid = incid),
+    "incid\\$local\\[1\\] is >0 but must be 0")
+})
