55dc8c42594c166b26b3f3e218ae2799c8f2fb6e
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2026-05-02 06:35:40 +0100

    Add tests for check_times triggered errors

--- tests/testthat/test-make_config.R
+++ tests/testthat/test-make_config.R
@@ -36,3 +36,32 @@ test_that("make_config triggers process_I error for negative counts with dates",
     make_config(incid = incid_no_dates),
     "incid must contain only non negative integer values\\.")
 })
+
+test_that("make_config triggers errors through check_times ", {
+  ## error for mismatched window lengths
+  incid <- data.frame(local = c(0, 1, 2, 1, 0, 1, 2, 1), imported = 0)
+  expect_error(
+    make_config(incid = incid, t_start = c(2, 3), t_end = c(6)),
+    "t_start and t_end must have the same length\\.")
+
+
+  ## error when t_start exceeds t_end"
+  incid <- data.frame(local = c(0, 1, 2, 1, 0, 1, 2, 1), imported = 0)
+  expect_error(
+    make_config(incid = incid, t_start = c(2, 7), t_end = c(6, 6)),
+    "t_start\\[i\\] must be <= t_end\\[i\\] for all i\\.")
+
+
+  ## error for invalid t_start values"
+  incid <- data.frame(local = c(0, 1, 2, 1, 0, 1, 2, 1), imported = 0)
+  expect_error(
+    make_config(incid = incid, t_start = c(1, 3), t_end = c(6, 7)),
+    "t_start must be a vector of integers between 2 and the number of")
+
+
+  ## error for invalid t_end values", 
+  incid <- data.frame(local = c(0, 1, 2, 1, 0, 1, 2, 1), imported = 0)
+  expect_error(
+    make_config(incid = incid, t_start = c(2, 3), t_end = c(6, 8.5)),
+    "t_end must be a vector of integers between 2 and the number of")
+})
