57ebf0cb51444790f1d2495e6478f728c73cf890
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2026-05-02 06:06:21 +0100

    Add test for make_config-process_I

--- tests/testthat/test-make_config.R
+++ tests/testthat/test-make_config.R
@@ -6,3 +6,10 @@ test_that("make_config triggers error when given method argument", {
     make_config(method = "uncertain_si"),
     msg)
 })
+
+test_that("make_config triggers process_I error for invalid incid columns", {
+  incid <- data.frame(foo = 1:5, bar = 6:10)
+  expect_error(
+    make_config(incid = incid),
+    "incid must be a vector or a dataframe with either i\\) a column")
+})
