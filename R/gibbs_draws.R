e422df5e0ed32c1bf8b3bdfe6f8021bcacbdf7bd
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2026-07-19 07:57:35 +0100

    Reuse example set-up in mv-epiestim

--- R/gibbs_draws.R
+++ R/gibbs_draws.R
@@ -239,18 +239,8 @@ draw_epsilon <- function(R, incid, lambda, priors,
 #'
 #' @export
 #'
+#' @example man/examples/multivariant_setup.R
 #' @examples
-#' n_v <- 2
-#' n_loc <- 3 # 3 locations
-#' T <- 100 # 100 time steps
-#' priors <- default_priors()
-#' # constant incidence 10 per day everywhere
-#' incid <- array(10, dim = c(T, n_loc, n_v))
-#' incid <- process_I_multivariant(incid)
-#' # arbitrary serial interval, same for both variants
-#' w_v <- c(0, 0.2, 0.5, 0.3)
-#' si_distr <- cbind(w_v, w_v)
-#' lambda <- compute_lambda(incid, si_distr)
 #' # Epsilon = 1 i.e. no transmission advantage
 #' epsilon <- 1
 #' draw_R(epsilon, incid$local, lambda, priors, seed = 1, t_min = 2L)
