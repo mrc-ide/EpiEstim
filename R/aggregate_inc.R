2163e63aed96563e5b234605bdfd4636f1309abe
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2026-09-12 06:34:47 +0100

    Use .var and {} to improve messages

--- R/aggregate_inc.R
+++ R/aggregate_inc.R
@@ -24,12 +24,12 @@
 
 aggregate_inc <- function(incid, dt = 7L)
 {
-  if(all(dt < 2)) {cli::cli_abort("at least one value of dt should be an integer >=2")}
+  if(all(dt < 2)) {cli::cli_abort("at least one value of {.var n} should be an integer >=2")}
   if (!all(is.integer(dt))) {
-    cli::cli_abort("dt should be an integer or vector of integers e.g. 2L or c(2L,2L,3L)")
+    cli::cli_abort("{.var dt} should be an integer or vector of integers e.g. 2L or c(2L,2L,3L)")
   }
   if (!is.vector(incid)) {
-    cli::cli_abort("incid should be a vector of integer values")
+    cli::cli_abort("{.var incid} should be a vector of integer values")
   }
   
   ndays <- length(incid)
@@ -46,7 +46,7 @@ aggregate_inc <- function(incid, dt = 7L)
     }
   }
   
-  message("Incidence aggregated up to day ", end[length(end)], " of ", ndays)
+  cli::cli_alert_success("Incidence aggregated up to day {end[length(end)]} of {ndays}")
   
   agg_inc <- numeric(length = length(start))
   
@@ -55,5 +55,6 @@ aggregate_inc <- function(incid, dt = 7L)
   }
   
   agg_inc <- as.integer(paste(agg_inc))
+
   agg_inc
 }
